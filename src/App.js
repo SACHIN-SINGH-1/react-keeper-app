1f6eded88f2c12b960840cc77294cd0b344b3976
SACHIN-SINGH-1 <zanepeter42@gmail.com>
2025-01-11 14:17:47 +0530

    Changes In App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./assets/App.css";
 import KeeperApp from "./components/KeeperApp";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 
 
 function App() {
   return (
+    <div>
     <KeeperApp/>
-    // <div>
-    //   <Router basename="/react-keeper-app">
-    //       <Routes>
-    //         <Route path='/' element={<center><h1>Welcome Home!!</h1></center>} />
-    //         <Route path='/keeper-app' element={<KeeperApp />} />
-    //         <Route path='/test' element={'It is a Test Route!'} />
-    //       </Routes>
-    //     </Router>
-    // </div>
+      <Router basename="/react-keeper-app">
+          <Routes>
+            <Route path='/' element={<center><h1>Welcome Home!!</h1></center>} />
+            <Route path='/keeper-app' element={<KeeperApp />} />
+            <Route path='/test' element={'It is a Test Route!'} />
+          </Routes>
+        </Router>
+    </div>
   );
 }
 
