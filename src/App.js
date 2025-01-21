7ed621b44fa4d5b1f9059de0da564ce5bef8214a
SACHIN-SINGH-1 <zanepeter42@gmail.com>
2025-01-21 22:55:42 +0530

    Changed Folder Name From Assets to Styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./assets/App.css";
+import "./styles/App.css";
 import KeeperApp from "./components/KeeperApp";
 
 
